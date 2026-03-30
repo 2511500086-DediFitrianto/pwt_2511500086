d74ddf1d9b7be5ce872b4e779407a7e4ccec7129
Annisa-2522500059 <2522500059@mahasiswa.atmaluhur.ac.id>
2026-03-30 16:05:40 +0700

    latihan ibuk

---
+++ Tampilan_awal.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPKK</title>
+    <link rel="stylesheet" href="style 2.css">
+</head>
+<body>
+
+<section class="hero">
+    <div class="hero-text">
+        <h1>Perkenalkan, Saya <span>Dedi Fitrianto</span></h1>
+        <p>Mau lebih mengenal saya? Klik tombol di bawah.</p>
+        <button onclick="location.href='login.php'">Login</button>
+    </div>
+
+    <div class="hero-image">
+        <img src="img.png" alt="Ilustrasi">
+    </div>
+</section>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+include "config/koneksi.php";
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = mysqli_query($conn,"SELECT * FROM admin WHERE username='$username' AND password='$password'");
+    $cek = mysqli_num_rows($query);
+
+    if($cek > 0){
+        $_SESSION['username']=$username;
+        header("location:starter.php");
+    }else{
+        echo "<script>alert('Username atau Password salah');</script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta churset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="Style.css">
+        <style>
+         body {
+            background-image: url('Background-01.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+</style>
+    </head>
+    <body>
+        <header>
+            <h2 class="logo"><img src="logo.png" alt="Logo"></h2>
+            <nav class="navigation">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+        <div class="container">
+            <div class="login">
+                <form method="POST">
+                    <h1>Login</h1>
+                    <hr>
+                    <p>Work From Office</p>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="Masukkan Username">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Password">
+                    <button type="submit" name="login">Login</button>
+                    <p>
+                        <a href="#">Forgot Password</a>
+                    </p>
+                    <div class="social-icons">
+                        <a href="#" class="icon google" title="Login with Google">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" alt="Google" style="width:24px;height:24px;">
+                        </a>
+                        <a href="#" class="icon facebook" title="Login with Facebook">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg" alt="Facebook" style="width:24px;height:24px;">
+                        </a>
+                        <a href="#" class="icon twitter" title="Login with Twitter">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" alt="Twitter" style="width:24px;height:24px;">
+                        </a>
+                    </div>
+                </form>
+            </div>
+            <div class="right">
+                <img src="img.png" alt="">
+            </div>
+        </div>
+    </body></html>
